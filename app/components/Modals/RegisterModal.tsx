9a9d8f821ade851696d0245a4dd1e636d0b169bb
calvin <ncalvin67@gmail.com>
2023-04-13 15:10:23 +0300

    :heavy_plus_sign: :art: Secondary registration modes added

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import Modal from "./Modal";
 import { title } from "process";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -34,7 +36,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error(error.message);
       })
       .finally(() => {
         setIsLoading(false);
@@ -43,47 +45,75 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-        <Heading
-            title="Welcome to airbnb"
-            subtitle="Create an account"
-        />
-        <Input
-            id="email"
-            label="Email"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
-        <Input
-            id="name"
-            label="Name"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
-        <Input
-            id="password"
-            type="password"
-            label="Password"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
+      <Heading title="Welcome to airbnb" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
-  )
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
-  <Modal 
-    isOpen={registerModal.isOpen} 
-    title="Register"
-    actionLabel="Continue"
-    onClose={registerModal.onClose}
-    onSubmit={handleSubmit(onSubmit)}
-    body={bodyContent}
-    disabled={isLoading} />
+    <Modal
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+      disabled={isLoading}
+    />
   );
 };
 
